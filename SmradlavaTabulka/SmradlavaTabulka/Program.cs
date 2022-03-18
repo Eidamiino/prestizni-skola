e2ae41ad974f6a0b28a384a2ff1f6552f920c207
Eidamiino <adam.pala199@gmail.com>
2022-03-18 17:55:30 +0100

    Tabulka smradu

--- SmradlavaTabulka/SmradlavaTabulka/Program.cs
+++ SmradlavaTabulka/SmradlavaTabulka/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Tabulka smradu:\n***************************");
+smrad("Adam Pala");
+smrad("Adam Hanzlík");
+smrad("Jan Rada");
+smrad("Adam Kijonka");
+smrad("Mirek Přeček");
+smrad("Matěj Bartusek");
+smrad("Ondřej Valenta");
+
+
+
+
+static void smrad(string jmeno)
+{
+    int suma = 0, delka = jmeno.Length;
+    double prumer;
+    for (int i = 0; i < delka; i++)
+    {
+        suma += jmeno[i];
+    }
+    prumer = (double) suma / delka;
+    prumer = Math.Round(prumer);
+    if (prumer % 7 == 0)
+    {
+        Console.WriteLine($"{jmeno}\tsmrdí jako bolavá noha bezdomovce");
+    }
+    else if (prumer % 5 == 0)
+    {
+        Console.WriteLine($"{jmeno}\tsmrdí jako koňská řiť");
+    }
+    else if (prumer % 3 == 0)
+    {
+        Console.WriteLine($"{jmeno}\tsmrdí jako cibuláči");
+    }
+    else
+    {
+        Console.WriteLine($"{jmeno}\tnesmrdí dost");
+    }
+}
